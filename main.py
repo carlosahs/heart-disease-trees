a3b2b50083582c0cba937108258725670a7e00f1
Carlos Hurtado <52082645+carlosahs@users.noreply.github.com>
2022-09-09 16:41:12 -0500

    Initialize repository

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+
